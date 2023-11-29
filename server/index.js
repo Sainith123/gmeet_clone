c8efc338bd6f4c064f7078422d1e1838dba7c13f
Sainith123 <yamsanisainith@gmail.com>
2023-11-29 23:32:12 +0530

    fc2

--- server/index.js
+++ server/index.js
@@ -92,6 +92,7 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
+
